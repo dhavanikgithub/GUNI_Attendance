8d6b7fdf47e090030569651618c38bb9419be1ae
Parth <parthbhuva97@gmail.com>
2023-01-26 13:43:26 +0530

    QR Code to TakeAttendance Fragment. Has some issues

--- app/src/main/java/com/example/guniattendance/ScannerActivity.kt
+++ app/src/main/java/com/example/guniattendance/ScannerActivity.kt
@@ -2,6 +2,8 @@ package com.example.guniattendance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.Toast
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
@@ -9,17 +11,20 @@ import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+import com.example.guniattendance.databinding.ActivityScannerBinding
+import com.example.guniattendance.student.studentfragments.ui.takeattendance.TakeAttendanceFragment
+import dagger.hilt.android.AndroidEntryPoint
 
 class ScannerActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
     private lateinit var scannerView : CodeScannerView
+    private lateinit var FLayout : FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
-
         scannerView = findViewById(R.id.scanner_view)
         codeScanner = CodeScanner(this, scannerView)
-
+        FLayout = findViewById(R.id.FLayout)
         initScanner()
 
     }
@@ -37,6 +42,8 @@ class ScannerActivity : AppCompatActivity() {
                 runOnUiThread {
                     //Code to Next Activity i.e. StudentHome Should be here
                     Toast.makeText(this@ScannerActivity,it.text,Toast.LENGTH_SHORT).show()
+                    FLayout.visibility = View.VISIBLE
+                    supportFragmentManager.beginTransaction().replace(R.id.FLayout, TakeAttendanceFragment()).commit()
                 }
             }
 
