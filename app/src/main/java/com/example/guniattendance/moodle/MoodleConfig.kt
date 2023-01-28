6272d5d193ab5f087c53982d7d265db1eb9ada18
Parth <parthbhuva97@gmail.com>
2023-01-28 14:58:50 +0530

    Made Moodle Config Url Dynamic

--- app/src/main/java/com/example/guniattendance/moodle/MoodleConfig.kt
+++ app/src/main/java/com/example/guniattendance/moodle/MoodleConfig.kt
@@ -1,11 +1,20 @@
 package com.example.guniattendance.moodle
 
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+import com.example.guniattendance.GUNIAttendanceSystem
 import com.uvpce.attendance_moodle_api_library.repo.ModelRepository
 
 class MoodleConfig {
+
     companion object{
-        var Url = "http://202.131.126.214"
-        fun getModelRepo(context: Context):ModelRepository {return ModelRepository(context,Url)}
+        var url = "http://202.131.126.214"
+        fun getModelRepo(context: Context):ModelRepository {
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+            url = sharedPreferences.getString("ServerList", "202.131.126.214")!!
+            return ModelRepository(context,"http://+$url")
+        }
     }
+
 }
